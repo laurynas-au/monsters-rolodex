c41a9dd5273816a8010cfe08a2c7a1216302ad41
Laurynas <laurynas.aukstuolis@gmail.com>
2020-06-10 21:59:44 +0300

    Updates

---
+++ precache-manifest.5d3d0436edc31433e0ff88f6a45b787f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cb233368f11085eafd1623e35d5d974",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1e1d5520271a8c3465b0",
+    "url": "/monsters-rolodex/static/css/main.87ee47eb.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e1d5520271a8c3465b0",
+    "url": "/monsters-rolodex/static/js/main.13320e3d.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
